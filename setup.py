600b8b13b33d7294fbf4f1791767d002935a0d1d
kelsensantos <kelsensantos@gmail.com>
2023-10-01 15:10:35 -0300

    Publicação de pacote

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 
 
-# with open("app/README.md", "r") as f:
-#     long_description = f.read()
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 
 def read_file(file):
@@ -17,9 +17,9 @@ setup(
     version=version,
     description='Modelos pré treinados para aplicação de PLN em textos jurídicos.',
     packages=find_packages(),
-    # long_description=long_description,
-    # long_description_content_type="text/markdown",
-    url='https://github.com/kelsensantos/juspln/juspln',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/kelsensantos/juspln/',
     author="kelsensantos",
     author_email="kelsensantos@gmail.com",
     license="MIT",
@@ -28,9 +28,5 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Operating System :: OS Independent",
     ],
-    # install_requires=[],
-    # extras_require={
-        # "dev": ["pytest>=7.0", "twine>=4.0.2"],
-    # },
     python_requires=">=3.9",
 )
